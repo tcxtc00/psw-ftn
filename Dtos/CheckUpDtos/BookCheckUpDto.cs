de930b6d5a1479a1df7d6de89f35426055ec2aa1
tcxtc00 <tanjaney0502@gmail.com>
2022-06-12 16:56:06 +0200

    fix bookCheckup typo

--- Dtos/CheckUpDtos/BookCheckUpDto.cs
+++ Dtos/CheckUpDtos/BookCheckUpDto.cs
@@ -4,7 +4,7 @@ namespace psw_ftn.Services.CheckUpService
 {
     public class BookCheckUpDto
     {
-        public int ChechUpId { get; set; }
+        public int CheckUpId { get; set; }
         public int PatientId { get; set; }
     }
 }
